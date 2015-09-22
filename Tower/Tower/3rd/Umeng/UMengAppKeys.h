6b490542ad0b811d317b3d02fb39241459817d21
Jonear <netease@NeteasedeMac-Pro-4.local>
2015-09-22 20:37:30 +0800

    commit

---
+++ Tower/Tower/3rd/Umeng/UMengAppKeys.h
@@ -0,0 +1,17 @@
+//
+//  UMengAppKeys.h
+//  CropMusic
+//
+//  Created by Jonear on 15/1/2.
+//  Copyright (c) 2015年 Jonear. All rights reserved.
+//
+
+#ifndef _UMengAppKeys_h
+#define _UMengAppKeys_h
+
+// 友盟
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#define UM_APPKEY @"54c9efb1fd98c5bb93000581"
+
+
+#endif
---
+++ Tower/Tower/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tower
+//
+//  Created by Jonear on 14-5-10.
+//  Copyright (c) 2014年 Jonear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TW_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TW_AppDelegate class]));
+    }
+}
